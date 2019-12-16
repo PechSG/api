6381214ea2247deb4e0b5ff195c780cb24e3b0fd
Robotboyx <vuthy.v@ligercambodia.org>
2019-12-16 12:19:41 +0700

    Change some sever code

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
@@ -56,6 +57,14 @@ const usersRouter = require('./routes/api/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/api', usersRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('client/build'));
+
+  app.get("*", (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+  })
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
