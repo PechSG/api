b6f0523634e136d716fe7ba97fe6bf16c9965c27
Narak Yim <narak.github@gmail.com>
2020-01-16 09:12:26 +0700

    Apply "Theme" To Old "Login Component"

--- client/src/components/login-user.component.js
+++ client/src/components/login-user.component.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import { Link } from 'react-router-dom';
+import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
 
 export default class LoginUser extends Component {
   constructor(props) {
@@ -60,7 +62,13 @@ export default class LoginUser extends Component {
 
     console.log(loginDetail);
 
- 
+    axios.post('http://localhost:8080/api/login', loginDetail)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
   axios.post('http://localhost:8080/api/login', loginDetail)
   .then(function (response) {
@@ -84,45 +92,108 @@ export default class LoginUser extends Component {
     });
   }
 
-  render() {    
-    return (
-    <div>
-      <h3>Login</h3>
-      <form onSubmit={this.onSubmit}>
-        <div className="form-group"> 
-          <label>Phone Number: </label>
-          <input  type="number"
-                required
-                className="form-control"
-                name="phoneNumber"
-                value={this.state.phoneNumber}
-                onChange={this.onChangePhoneNumber}
-                />
-        </div>
-        <div className="form-group"> 
-            <label>Password: </label>
-            <input type="password"
-                required
-                className="form-control"
-                value={this.state.password}
-                onChange={this.onChangePassword}
-                />
-          </div> 
-        {/* <div className="form-group">
-          <label>Date: </label>
-          <div>
-            <DatePicker
-              selected={this.state.date}
-              onChange={this.onChangeDate}
-            />
-          </div>
-        </div> */}
+//   render() {    
+//     return (
+//     <div>
+//       <h3>Login</h3>
+//       <form onSubmit={this.onSubmit}>
+//         <div className="form-group"> 
+//           <label>Phone Number: </label>
+//           <p className="text-muted">Sign In to your account</p>
+//           <input  type="number"
+//                 required
+//                 className="form-control"
+//                 name="phoneNumber"
+//                 value={this.state.phoneNumber}
+//                 onChange={this.onChangePhoneNumber}
+//                 />
+//         </div>
+//         <div className="form-group"> 
+//             <label>Password: </label>
+//             <input type="password"
+//                 required
+//                 className="form-control"
+//                 value={this.state.password}
+//                 onChange={this.onChangePassword}
+//                 />
+//           </div> 
+//         {/* <div className="form-group">
+//           <label>Date: </label>
+//           <div>
+//             <DatePicker
+//               selected={this.state.date}
+//               onChange={this.onChangeDate}
+//             />
+//           </div>
+//         </div> */}
 
-        <div className="form-group">
-          <input type="submit" value="Login" className="btn btn-primary" />
-        </div>
-      </form>
+//         <div className="form-group">
+//           <input type="submit" value="Login" className="btn btn-primary" />
+//         </div>
+//       </form>
+//     </div>
+//     )
+//   }
+// }
+
+render() {
+  return (
+    <div className="app flex-row align-items-center">
+      <Container>
+        <Row className="justify-content-center">
+          <Col md="8">
+            <CardGroup>
+              <Card className="p-4">
+                <CardBody>
+                  <Form>
+                    <h1>Login</h1>
+                    <p className="text-muted">Sign In to your account</p>
+                   
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-user"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="number" placeholder="Phone Number" autoComplete="phoneNumber" />
+                    </InputGroup>
+                    <InputGroup className="mb-4">
+
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-lock"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="password" placeholder="Password" autoComplete="current-password" />
+                    </InputGroup>
+                    
+                    <Row>
+                      <Col xs="6">
+                        <Button color="primary" className="px-4">Login</Button>
+                      </Col>
+                      <Col xs="6" className="text-right">
+                        <Button color="link" className="px-0">Forgot password?</Button>
+                      </Col>
+                    </Row>
+                  </Form>
+                </CardBody>
+              </Card>
+              <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
+                <CardBody className="text-center">
+                  <div>
+                    <h2>Sign up</h2>
+                    <p>Hello Bong Vu</p>
+                    <Link to="/register">
+                      <Button color="primary" className="mt-3" active tabIndex={-1}>Register Now!</Button>
+                    </Link>
+                  </div>
+                </CardBody>
+              </Card>
+            </CardGroup>
+          </Col>
+        </Row>
+      </Container>
     </div>
-    )
-  }
+  );
+}
 }
