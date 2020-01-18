b622972f0cdda7b2bd8a19a3916d3f9719d56750
Robotboyx <vuthy.v@ligercambodia.org>
2020-01-18 10:41:18 +0700

    Change to exact

--- client/src/App.js
+++ client/src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
               <Route exact path="/registers" name="Register Page" render={props => <RegisterUser {...props}/>} />
               <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
               <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-              <Route path="/" name="Home" render={props => <DefaultLayout {...props}/>} />
+              <Route exact path="/" name="Home" render={props => <DefaultLayout {...props}/>} />
           
             
             </Switch>
